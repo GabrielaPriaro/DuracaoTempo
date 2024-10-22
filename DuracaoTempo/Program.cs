0d65ab522625bdb8b3c5d0bcd5c211f2ef5c4fdd
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 17:53:56 +0100

    Implementaçao para transformar total de segundos em formato hora.

--- DuracaoTempo/Program.cs
+++ DuracaoTempo/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace DuracaoTempo
+﻿//Problema "duracao"
+//Fazer um programa para ler uma duração de tempo em segundos, daí imprimir na tela esta duração no
+//formato horas:minutos:segundos.
+
+namespace DuracaoTempo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int totalSegundos, duracaoHoras, duracaoMinutos, duracaoSegundos ;
+
+            Console.Write("Digite a duracao em segundos: ");
+            totalSegundos = int.Parse(Console.ReadLine());
+
+            duracaoHoras = totalSegundos / 3600;
+            duracaoMinutos = (totalSegundos % 3600) / 60;
+            duracaoSegundos = totalSegundos % 60;
+
+            Console.WriteLine($"{duracaoHoras}:{duracaoMinutos}:{duracaoSegundos}");
         }
     }
 }
