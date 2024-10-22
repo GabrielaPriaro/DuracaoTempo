22a5856539028f23917d4ccb26bac588dd073530
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 17:40:35 +0100

    Adicionar arquivos de projeto.

---
+++ DuracaoTempo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DuracaoTempo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
